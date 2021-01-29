33c7f9d55d1368df41bea169740665e71c551d96
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2021-01-29 13:13:09 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -29,16 +29,16 @@ os.system('mkdir /input/mat')
 
 flair_path = context.get_input_path('FLAIR')
 path = os.path.dirname(flair_path) # get the path minus the filename
-newname = os.path.join(path, 'FLAIR_INPUT.nii.gz') # new name is path plus 'FLAIR_INPUT.nii.gz'
+newname = os.path.join(path, 'FLAIR.nii.gz') # new name is path plus 'FLAIR_INPUT.nii.gz'
 os.rename(flair_path, newname) # do the rename
 
 t1_path = context.get_input_path('T1')
 path = os.path.dirname(t1_path) # get the path minus the filename
-newname = os.path.join(path, 'T1.nii.gz') # new name is path plus 'T1.nii.gz'
+newname = os.path.join(path, 'T1_INPUT.nii.gz') # new name is path plus 'T1.nii.gz'
 os.rename(t1_path, newname) # do the rename
 
 # Transform using FSL
-os.system('/usr/share/fsl/bin/flirt -in /flywheel/v0/input/FLAIR/FLAIR_INPUT.nii.gz -ref /flywheel/v0/input/T1/T1.nii.gz -out /flywheel/v0/input/FLAIR/FLAIR.nii.gz -dof 6 -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -omat /input/mat/FLAIR2T1.mat') #possible straight directory for -out flag
+os.system('/usr/share/fsl/bin/flirt -in /flywheel/v0/input/T1/T1_INPUT.nii.gz -ref /flywheel/v0/input/FLAIR/FLAIR.nii.gz -out /flywheel/v0/input/T1/T1.nii.gz -dof 6 -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -omat /input/mat/T12FLAIR.mat') #possible straight directory for -out flag
 
 for fname in os.listdir('/flywheel/v0/input'):
       if os.path.isfile(os.path.join('/flywheel/v0/input/FLAIR', 'FLAIR.nii.gz')):
@@ -48,13 +48,13 @@ for fname in os.listdir('/flywheel/v0/input'):
 
 # Commands
 os.system("python /wmhseg_example/example.py")
-os.system('/opt/convert3d-nightly/bin/c3d /input/pre/T1.nii.gz /output/result.nii.gz -copy-transform -o /output/result.nii.gz')
-shutil.copy(os.path.join('/output', 'result.nii.gz'), os.path.join('/flywheel/v0/output', output_file_T1_name))
+os.system('/opt/convert3d-nightly/bin/c3d /input/pre/FLAIR.nii.gz /output/result.nii.gz -copy-transform -o /output/result.nii.gz')
+shutil.copy(os.path.join('/output', 'result.nii.gz'), os.path.join('/flywheel/v0/output', output_file_FLAIR_name))
 
 
 
 # convert to FLAIR space and write
-os.system('/usr/share/fsl/bin/convert_xfm -omat /input/mat/T12FLAIR.mat -inverse /input/mat/FLAIR2T1.mat')
-os.system('/usr/share/fsl/bin/applywarp --ref=/flywheel/v0/input/FLAIR/FLAIR_INPUT.nii.gz --in=/output/result.nii.gz --out=/output/resultFLAIR.nii.gz --premat=/input/mat/T12FLAIR.mat --interp=nn')
-shutil.copy(os.path.join('/output','resultFLAIR.nii.gz'), os.path.join('/flywheel/v0/output', output_file_FLAIR_name))
+os.system('/usr/share/fsl/bin/convert_xfm -omat /input/mat/FLAIR2T1.mat -inverse /input/mat/T12FLAIR.mat')
+os.system('/usr/share/fsl/bin/applywarp --ref=/flywheel/v0/input/T1/T1_INPUT.nii.gz --in=/output/result.nii.gz --out=/output/resultT1.nii.gz --premat=/input/mat/FLAIR2T1.mat --interp=nn')
+shutil.copy(os.path.join('/output','resultT1.nii.gz'), os.path.join('/flywheel/v0/output', output_file_T1_name))
 
